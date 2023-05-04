2a56804c7f0ce4166a9349f8d48fa8e8092ae311
snowm <snowman4453@gmail.com>
2023-05-04 12:11:52 +0900

    Initial commit

---
+++ src/main/java/com/example/somsom_market/SomSomMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.somsom_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomSomMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomSomMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/somsom_market/SomSomMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.somsom_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomSomMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
