09894222543ead517dca911150acd2b533fcb857
monica <somamonica01@gmail.com>
2023-05-12 15:11:48 +0900

    GroupItem 수정 GroupItemController 수정 GroupItemDao 추가 (하다보니까 ItemDao 만으로는 부족할 것 같아서...) GroupItemService 추가 Review domain class 추가 ReviewController 수정 ReviewDao 추가 ReviewRequest 수정 ReviewService 추가

--- src/main/java/com/example/somsom_market/controller/Review/ReviewController.java
+++ src/main/java/com/example/somsom_market/controller/Review/ReviewController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class ReviewController {
