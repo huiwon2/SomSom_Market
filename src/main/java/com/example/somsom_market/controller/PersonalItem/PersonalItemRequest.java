6cfabedce07b3f41cfd94bf4fbc2f37c7ee9b597
ckdmstn <eunsu321@naver.com>
2023-05-17 14:44:27 +0900

    [ADD] PersonalItemRequest 추가

---
+++ src/main/java/com/example/somsom_market/controller/PersonalItem/PersonalItemRequest.java
@@ -0,0 +1,14 @@
+package com.example.somsom_market.controller.PersonalItem;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class PersonalItemRequest {
+    private String title;
+    private int price;
+    private String description;
+    private List<String> imageUrl;
+    private String status; // 거래가능 / 거래중 / 거래완료
+}
