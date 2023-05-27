10c873c924acaac7ba7a3181c1a00e653b502681
monica <somamonica01@gmail.com>
2023-05-27 23:09:41 +0900

    [CHORE]GroupItemController, GroupItemDao, ReviewDao, GroupItemService 수정 [FEAT]ReviewRepository, GroupItemRepository 생성

--- src/main/java/com/example/somsom_market/dao/GroupItemDao.java
+++ src/main/java/com/example/somsom_market/dao/GroupItemDao.java
@@ -1,14 +1,40 @@
 package com.example.somsom_market.dao;
 
 import com.example.somsom_market.domain.GroupItem;
+import org.springframework.stereotype.Repository;
 
-public interface GroupItemDao extends ItemDao{
-    GroupItem getItem(String itemId);
-
-    String insertGroupItem(GroupItem groupItem);
-
-    String updateGroupItem(GroupItem groupItem);
-
-    void deleteGroupItem(String itemId);
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+@Repository
+public class GroupItemDao extends ItemDao{
+    @PersistenceContext
+    private EntityManager em;
+    @Transactional
+    public GroupItem getItem(String itemId){
+        GroupItem groupItem = em.find(GroupItem.class, itemId);
+        return groupItem;
+    }
+    @Transactional
+    public String insertGroupItem(GroupItem groupItem){
+        em.persist(groupItem);
+        String itemId = groupItem.getItemId();
+        return itemId;
+    }
+    @Transactional
+    public String updateGroupItem(GroupItem groupItem){
+        em.merge(groupItem);
+        String itemId = groupItem.getItemId();
+        return itemId;
+    }
+    @Transactional
+    public void deleteGroupItem(String itemId){
+        GroupItem groupItem = em.find(GroupItem.class, itemId);
+        if(em.contains(groupItem)){
+            em.remove(groupItem);
+        }else {
+            em.remove(em.merge(groupItem));
+        }
+    }
 
 }
--- src/main/java/com/example/somsom_market/service/ReviewService.java
+++ src/main/java/com/example/somsom_market/service/ReviewService.java
@@ -3,17 +3,21 @@ package com.example.somsom_market.service;
 import com.example.somsom_market.controller.Review.ReviewRequest;
 import com.example.somsom_market.dao.ReviewDao;
 import com.example.somsom_market.domain.Review;
+import com.example.somsom_market.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class ReviewService {
     @Autowired
     ReviewDao reviewDao;
 
+    @Autowired
+    ReviewRepository reviewRepository;
     //userId로 리뷰 리스트 반환
     public List<Review> getReviewListByUserId(int userId){
-        return reviewDao.getReviewList(userId);
+        return reviewRepository.findReviewByUserId(userId);
     }
 
     private static Review reqToReview(ReviewRequest req){
