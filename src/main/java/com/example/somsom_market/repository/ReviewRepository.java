db870b79724748c54ac4cba04b865c355ccc5f92
ckdmstn <eunsu321@naver.com>
2023-06-17 18:01:03 +0900

    [CHORE] 회원 탈퇴 수정 (도메인 매핑 이름만 같이 수정했습니다)

--- src/main/java/com/example/somsom_market/repository/ReviewRepository.java
+++ src/main/java/com/example/somsom_market/repository/ReviewRepository.java
@@ -2,11 +2,10 @@ package com.example.somsom_market.repository;
 
 import com.example.somsom_market.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findReviewByUserId(String userId); // userId로 리뷰 리스트 반환
-//    void deleteByAccountId(String id);
+    void deleteByUserId(String id);
 }
